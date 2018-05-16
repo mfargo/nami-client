64a97c2427a86d66d3863d73e7e0a7046b04e82d
Matthew Fargo <mfargo@gmail.com>
2018-05-16 10:54:28 +0900

    Initial Commit

---
+++ NamiClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NamiClient
+//
+//  Created by Matthew Fargo on 2018/05/16.
+//  Copyright © 2018年 Matthew Fargo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NamiClientTests/NamiClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  NamiClientTests.swift
+//  NamiClientTests
+//
+//  Created by Matthew Fargo on 2018/05/16.
+//  Copyright © 2018年 Matthew Fargo. All rights reserved.
+//
+
+import XCTest
+@testable import NamiClient
+
+class NamiClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
